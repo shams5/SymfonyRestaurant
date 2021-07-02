8c07b3ddea6da7d957e94d93739cf8a949de96fe
shams git <shamskhurasani481@gmail.com>
2021-07-02 10:18:27 +0200

    Creating a new instance in PlatFixtures.php and loaded it on the database by runing: php bin/console doctrine:fixtures:load in power shell

--- src/DataFixtures/PlatFixtures.php
+++ src/DataFixtures/PlatFixtures.php
@@ -5,13 +5,18 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class PlatFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+    //   Création de notre première instance d'Entity Plat
+    $plat = new \App\Entity\Plat;
+    $plat->setName('Pizza');
+    $plat->setDescription('Ceci est une Pizza');
+    $plat->setPrice(19.99);
+    // Demande de persistance (cad enregistrement dans la base de données, faisant ainsi PERSISTER notre instance d'Entity au-delà de la session utilisateur)
+    $manager->persist($plat);
+    // Exécution des requetes déposées
+    $manager->flush();
     }
 }
