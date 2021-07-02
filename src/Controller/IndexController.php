10a3c7321234991959ee3020882260545b7e39f4
shams git <shamskhurasani481@gmail.com>
2021-07-02 12:20:37 +0200

    Preparation of index function in IndexController for git data of two table of database Plat and Category

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -13,8 +13,23 @@ class IndexController extends AbstractController
      */
     public function index(): Response
     {
+        // Nous récuperons l'Entity Managr afin de pouvoir diallogeur avec notre base de donées
+        // Nous récuperons égalemnet le Repository de Plat pour récuperer des éléments de cette table
+        $entityManager = $this->getDoctrine()->getManager();
+        $platRepository = $entityManager->getRepository(\App\Entity\Plat::class);
+        // Nous récuperons la liste des Catégories
+        $categoryRepository = $entityManager->getRepository(\App\Entity\Category::class);
+        $categories = $categoryRepository->findAll();
+        // Nous récuperons tous les Plats
+        $plats = $platRepository->findAll();
+        // Nous mélangeons l'ordre des plats pour notre page d'accuiel en mélangeant les clefs du tableau reçu 
+        shuffle($plats);
+        // Nous ne recupérons que les huit prmèires entrées du tableua tout juste mélangé
+        $slicedPlats = array_slice($plats, 0, 8);
+        // Nous transmettons les différentes variables à notre vue Twig
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'categories' => $categories,
+            'plats' => $slicedPlats,
         ]);
     }
 }
